79e054ea2bd5c8cadac72251c8d52a86039ae06e
kuandry <cariocahdouglas@gmail.com>
2026-04-25 21:44:57 -0300

    Primeiro commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kuandry/curso-go
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/kuandry/curso-go/meet"
+)
+
+func main() {
+	meet.SayHello("Olá, Douglas!")
+	meet.Say("Olá, Maria!")
+}
